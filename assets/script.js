5e26aa644a994581245b281882ab12783fb75e6d
Marcel Hebing <marcel@ddionrails.org>
2017-07-25 17:40:07 +0200

    Update

--- assets/script.js
+++ assets/script.js
@@ -119,16 +119,4 @@ $(function () {
 
     szEmbedService.setHeight("800px");
 
-  var _paq = _paq || [];
-  _paq.push(["setDoNotTrack", true]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//piwik.hiig.de/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '11']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-
 });
