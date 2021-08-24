3d64520228a67180130255797ac8c8b5bef3e716
mhebing <marcel.hebing@gmail.com>
2021-08-24 18:37:51 +0200

    Update to 2021 version

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,12 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
-/* Reacts on mousehover over first headline and closes Navigation on click */
-
-//function suggestHideNavigation() {
-//    $('#filter .label-text').text('Themenauswahl einklappen');
-//    $('.collapsed #filter .label-text').text('Themen ändern');
-//}
-//
-//function suggestHideNavigationOut() {
-//    $('#filter .label-text').text('Ausgewählte Themen');
-//}
-//
-
-
-/* ============================================== DOCUMENT READY ========================================== */
+/* ================================================== DOCUMENT READY ================================================== */
 
 $(function () {
     
-    /* Hides first headline once filter is clicked */
-    
-    //$('#topAndSubCategories').click(
-    //    function () {
-    //        $('#navigationHeader').slideUp();
-    //    }
-    //);
-    
-    /* Reacts on mousehover over filter and closes Top And Sub Categories on click */
-    //$('#filter').mouseenter(
-    //    function () {
-    //        suggestHideNavigation();
-    //    }
-    //);
-    //
-    //$('#filter').mouseleave(
-    //    function () {
-    //        suggestHideNavigationOut();
-    //    }
-    //);
-    //
+    /* ============================================== Collapsing #topAndSubCategories ================================= */
+
     $('#filter').click(
         function (event) {
             var target = $(event.target);
@@ -48,29 +16,40 @@ $(function () {
         }
     );
 
-    $(".navbar-toggle").click(function() {
-        $navbar = $(".navbar");
-        if ($navbar.attr("open")) {
-            $navbar.removeAttr("open");
-        } else {
-            $navbar.attr("open", "true");
-        }
-    })
-    
-    /* Hides second headline once party is clicked + removes Debug Parties */
-    
-    $('content #parties .btn').click(
+    $(".navbar-toggle").click(
         function () {
-            $('#contentHeader').slideUp();
-            $('content').contents().filter(function () {
-                return this.nodeType === Node.TEXT_NODE;
+            $navbar = $(".navbar");
+            if ($navbar.attr("open")) {
+                $navbar.removeAttr("open");
+            } else {
+                $navbar.attr("open", "true");
             }
-                                          )[1].nodeValue = '';
         }
     );
     
+    // collapse on scroll
+
+    //FF
+    document.addEventListener('DOMMouseScroll', function (event) {
+        if ($('#topAndSubCategories[aria-expanded="true"]').length > 0
+                && $('#topAndSubCategories').parent().css('display') !== 'none'
+                && window.innerHeight > 600) {
+            $(".navbar-toggle").click();
+        }
+    }, true); /*Capture event*/
+
+    // Chrome and Safari
+    document.addEventListener('mousewheel', function (event) {
+        if ($('#topAndSubCategories[aria-expanded="true"]').length > 0
+                && $('#topAndSubCategories').parent().css('display') !== 'none'
+                && window.innerHeight > 600) {
+            $(".navbar-toggle").click();
+        }
+    }, true); /*Capture event*/
+
     
-    /* Hides Columns if no content under Subheadline */
+    
+    /* ============================================== Hides Columns if no content under Subheadline =================== */
     $('#subCategories .col-md-3:not(:has(p))').hide();
     $('#topCategories nav li').click(
         function () {
@@ -85,38 +64,43 @@ $(function () {
         }
     );
     
-    $('app-root').css('background','none #fff');
+    $('app-root').css('background', 'none #fff');
     
-    /* ============================================== Make headings clickable and labels sticky ======================== */
+
+
+    /* ============================================== Make headings clickable and labels sticky ======================= */
     
     /* Shows little-labels on click */
-    $('.wrapper-sentence').click(
-        function(){
-            $(this).toggleClass('expandTag');
-        }
-    );
+//    $('.wrapper-sentence').click(
+//        function () {
+//            $(this).toggleClass('expandTag');
+//        }
+//    );
     
     /* toggles wrapper-content with click on header */
-    $(':header').click(
-        function(){
-            $(this).next('.wrapper-inner').slideToggle();
+//    $(':header').click(
+//        function () {
+//            $(this).next('.wrapper-inner').slideToggle();
+//        }
+//    );
+
+    $("body").on(
+        "click",
+        ":header",
+        function () {
+            $(this).toggleClass("hidden-inner");
+            $(this).next(".wrapper-inner").toggle();
         }
     );
     
+    /* ============================================== Keeps loading gif a moment after page is loaded ================= */
+
     setTimeout(function () {
         //hide Loading Animation
         $('#loadingOverlay').fadeOut();
-    }, 1000);
+    }, 2000);
 
-    $("body").on(
-      "click",
-      ":header",
-      function(){
-        $(this).toggleClass("hidden-inner");
-        $(this).next(".wrapper-inner").toggle();
-      }
-    );
-
+    /* ============================================== for SZ EMBED script to adjust height ============================ */
     szEmbedService.setHeight("800px");
 
 });
